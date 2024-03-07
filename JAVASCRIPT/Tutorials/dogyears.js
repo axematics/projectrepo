5649abdb836583795b61b61e68b613469450e276
axematics <axel.thomas@outlook.com>
2024-03-07 21:06:48 +0100

    dog years exercise from Codecademy

---
+++ JAVASCRIPT/Tutorials/dogyears.js
@@ -0,0 +1,18 @@
+const myAge = 31;
+// myAge is a constant
+let earlyYears = 2;
+// this variable might change
+earlyYears *= 10.5;
+
+laterYears = myAge - 2;
+
+laterYears *= 4;
+
+console.log(laterYears);
+console.log(earlyYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+
+myName = 'Axel'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
