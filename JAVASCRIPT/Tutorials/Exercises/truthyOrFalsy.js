e09473c7faba6071d7cd5ddaebbf3ca00363bac8
axematics <axel.thomas@outlook.com>
2024-03-12 19:49:05 +0100

    truthyOrFalsy

---
+++ JAVASCRIPT/Tutorials/Exercises/truthyOrFalsy.js
@@ -0,0 +1,25 @@
+// Write your function here:
+
+function truthyOrFalsy(thing) {
+    if (thing) {
+      return true;
+    } else if (!thing) {
+      return false;
+    }
+  }
+  
+  
+  
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+  console.log(truthyOrFalsy(0)) // Should print false
+  
+  // We encourage you to add more function calls of your own to test your code!
+  
+  
+  
+  
+  
+  
+  
