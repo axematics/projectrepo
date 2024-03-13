aeef9eb703fbeca68be1a98dcb003771fce996f3
axematics <axel.thomas@outlook.com>
2024-03-13 17:02:04 +0100

    added code to script.js

--- JAVASCRIPT/number-guesser-starting/script.js
+++ JAVASCRIPT/number-guesser-starting/script.js
@@ -4,3 +4,35 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+    let x;
+    x = Math.floor(Math.random()*9);
+    return x;
+}
+
+const compareGuesses = (user,cpu,secret) => {
+    secret = generateTarget();
+    if (user == secret || Math.abs(secret - user) < Math.abs(secret - cpu) || secret - user == secret - cpu) {
+        return true;
+    } else if (cpu == secret || Math.abs(secret - user) > Math.abs(secret - cpu)) {
+        return false;
+    } else {
+        return 'Error';
+    }
+}
+
+// console.log(compareGuesses(2,7,9));
+
+const updateScore = winner => {
+    if (winner = 'human') {
+        humanScore++;
+    } else if (winner = 'computer') {
+        computerScore++;
+    } else {
+        return 'Player doesn\'t exist';
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber++;
+}
