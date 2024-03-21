860f7c342f3d964e97b59994966dd482415b6af3
axematics <axel.thomas@outlook.com>
2024-03-21 18:47:14 +0100

    willLikelySurvive() functional

--- JAVASCRIPT/mystery-organism-starter/main.js
+++ JAVASCRIPT/mystery-organism-starter/main.js
@@ -59,17 +59,36 @@ const pAequorFactory = (specimenNum, dna) => {
       console.log(counter);
       let percentage = ((counter/this.dna.length)*100);
       return `Degree of similarity between Specimen ${this.specimenNum} and Specimen ${pAequor.specimenNum} is ${percentage}%.`;
+    },
+    willLikelySurvive() {
+      let ifCorG = [];
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === 'C' || this.dna[i] === 'G') {
+          ifCorG += this.dna[i];
+        }
+      };
+      console.log(`Full list of C or G bases in this specimen: ${ifCorG}`);
+      let survivalChance = ((ifCorG.length / this.dna.length)*100);
+      console.log(`This specimen's survival chance is ${survivalChance}%.`);
+      if (survivalChance >= 60) {
+        return true;
+      } else {
+        return false;
+      }
     }
   }
 }
 
+const sample
+
+/*
 let thing = pAequorFactory(1234, ['A','T','C','G','A','G','C','A','A','G']);
-let thing2 = pAequorFactory(9999, ['T','T','C','G','T','A','C','T','C','G']);
+let thing2 = pAequorFactory(9999, ['C','C','C','G','T','A','C','C','C','G']);
 let thing3 = pAequorFactory(34287, ['T','T','T','T','T','T','T','T','T','T']);
 //console.log(thing);
 //console.log(thing.mutate());
 thing.mutate();
 console.log(thing.compareDNA(thing2));
 console.log(thing.compareDNA(thing3));
-
-
+console.log(thing2.willLikelySurvive());
+*/
